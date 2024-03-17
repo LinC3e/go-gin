47f9e236927de37e86ccde58e63357dae7a9cb8f
LinC3e <ivancostilla17@gmail.com>
2024-03-17 16:15:25 -0300

    Test - http req

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong2",
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "home",
 		})
 	})
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "home",
+	r.GET("/news", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "news",
+		})
+	})
+
+	r.POST("/add", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "add",
+		})
+	})
+
+	r.PUT("/edit", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "edit",
+		})
+	})
+
+	r.DELETE("/delete", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "delete",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
